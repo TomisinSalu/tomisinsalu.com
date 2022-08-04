c0dbbd2fdd5fe262c1125761cbfac7f460525a4e
Tomisin101 <salutomisin@gmail.com>
2022-08-04 11:55:22 -0400

    First Git Commit

---
+++ Assets/main.js
@@ -0,0 +1,22 @@
+//Change nav style on scroll
+
+// window.addEventListener('scroll', () =>{
+//     document.queryselector('nav').classList.toggle('nav-scroll'), window.scrollY > 0;
+// }
+// )
+const nav = document.querySelector('nav');
+
+window.addEventListener('scroll', function() { 
+    if(window.scrollY > 75)
+      nav.classList.add('scroll');
+    else 
+      nav.classList.remove('scroll');
+  });
+
+const title = document.getElementById("title")
+
+var animateText = function(){
+    strimgs
+}
+
+window.setInterval(animateText, 3000);
